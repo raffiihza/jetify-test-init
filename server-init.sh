151601d71e6490894cd85df21828e6a6c0b08343
Raffi Ihza Zuhairnawan <>
2024-11-18 15:49:50 +0000

    Almost final, either I'm exhausted or this weird Laravel can't connect to local MySQL is solved

--- server-init.sh
+++ server-init.sh
@@ -1,3 +1,5 @@
 sudo apt update
-sudo apt install -y docker-compose
+sudo apt install -y docker-compose php8.3-mysql php8.3-xml
+mkdir -p modul5/storage > /dev/null 2>&1
+chmod -R 777 modul5/storage > /dev/null 2>&1
 echo "Environment installation completed!"
