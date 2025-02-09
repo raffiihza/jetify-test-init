c333b2819b598b11b4c1baa0ea6dbaf9cc4a311a
Raffi Ihza Zuhairnawan <>
2025-02-09 06:38:17 +0000

    Forgot mysql client

--- server-init.sh
+++ server-init.sh
@@ -1,4 +1,4 @@
 sudo apt update
 export DEBIAN_FRONTEND=noninteractive
 # sudo apt install -y docker-compose
-sudo apt install -y php8.3-mysql php8.3-xml mysql-server
+sudo apt install -y php8.3-mysql php8.3-xml mysql-server mysql-client
